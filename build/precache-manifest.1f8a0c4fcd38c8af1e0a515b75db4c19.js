88507885e45a5bf8d4edb064da4ad37804a6f59e
kostDev <kostyaboy777@gmail.com>
2019-06-09 19:17:47 +0300

    load build

---
+++ build/precache-manifest.1f8a0c4fcd38c8af1e0a515b75db4c19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e364e82cf603212f180800d35fcc96f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5585f11f1c40a45adff9",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a1dc1f259bc0f03185f2",
+    "url": "/static/css/main.928bd444.chunk.css"
+  },
+  {
+    "revision": "5585f11f1c40a45adff9",
+    "url": "/static/js/2.3e35f59b.chunk.js"
+  },
+  {
+    "revision": "a1dc1f259bc0f03185f2",
+    "url": "/static/js/main.d5178f77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa5eeddaa5e5307ded23398a42565241",
+    "url": "/static/media/404page.fa5eedda.jpg"
+  },
+  {
+    "revision": "832f3cd5f59ac8311b618b7054b5a5ad",
+    "url": "/static/media/contact_us.832f3cd5.jpg"
+  },
+  {
+    "revision": "9b0625b1e776a51bb2d903a6791f65af",
+    "url": "/static/media/header.9b0625b1.png"
+  }
+]);
